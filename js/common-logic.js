4e5f65c64b77fae8dd87512fd82e4b2664041c78
diegobari06 <diegobari06@hotmail.com>
2014-07-12 01:44:20 -0600

    Actualizacion registrar curso

--- js/common-logic.js
+++ js/common-logic.js
@@ -26,3 +26,23 @@ function validarCorreo(pCorreo, pElementoError, pMsjError) {
     pElementoError.innerHTML=pMsjError;
     pElementoError.className += ' error';;
 }
+
+//validar que solo puedan introducirse letras//
+function soloLetras(e){
+       key = e.keyCode || e.which;
+       tecla = String.fromCharCode(key).toLowerCase();
+       letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
+       especiales = "8-37-39-46";
+
+       tecla_especial = false
+       for(var i in especiales){
+            if(key == especiales[i]){
+                tecla_especial = true;
+                break;
+            }
+        }
+
+        if(letras.indexOf(tecla)==-1 && !tecla_especial){
+            return false;
+        }
+    }

17063ff3cb54ac26b1f73e1896334b1904e5bfd4
KuzuriX <cotto527@gmail.com>
2014-07-11 20:51:10 -0600

    Agregando validaciones comunes de campos vacios y formato de correo

--- js/seguridad.js
+++ js/seguridad.js
@@ -1,10 +1,18 @@
 var eBtnIniciarSesion = document.querySelector('.btn-primary'),
     aInputs = document.querySelectorAll('form .form-control'),
-    eError = document.querySelector('.error');
+    eError = document.querySelector('.alert-error'),
+    eCorreo = document.querySelector('#password').value,
+    regis=false;
 
-eBtnIniciarSesion.addEventListener('click', function () {
+eBtnIniciarSesion.addEventListener('click', function (evento) {
     
-   validarCamposLlenos(aInputs, eError, 'Los campos deben estar llenos');
+    evento.preventDefault();
+    var camposVacios=validarCamposLlenos(aInputs, eError, 'Todos los campos deben estar llenos');
+    if(!camposVacios){
+        validarCorreo(eCorreo, eError, 'El correo es incorrecto');
+    }
     
+    
+
     
 }); /*addEvenListener = Observador del evento y function ejecuta la funcion dentro de  los corchetes cuando se ejecuta la funcion*/

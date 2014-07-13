9ec6bcce6b973ffe6e2e313c2b4959caf6ff0fc0
diegobari06 <diegobari06@hotmail.com>
2014-07-13 03:11:10 -0600

    Avance consultar cursos 3:10am

--- js/configuracion.js
+++ js/configuracion.js
@@ -1,12 +1,13 @@
-var ebtnCrear = document.querySelector('#btnCrearCurso'),
+var ebtnCrearCurso = document.querySelector('#btnCrearCurso'),
 	aInputs = document.querySelectorAll('form .form-control'),
 	eError = document.querySelector('.alert-error'),
 	regis=false,
-	ecodigo = document.querySelector('#txtCodCurso');
+	ecodigo = document.querySelector('#txtCodCurso'),
+  ebtnCrearCarrera = document.querySelector('#btnCrearCarrera');
 
 /* validar crear curso */
 
-ebtnCrear.addEventListener('click',function (evento) {
+ebtnCrearCurso.addEventListener('click',function (evento) {
 	
 	evento.preventDefault();
     var camposVacios=validarCamposLlenos(aInputs, eError, 'Todos los campos deben estar llenos');
@@ -14,25 +15,34 @@ ebtnCrear.addEventListener('click',function (evento) {
  
 	});
 
+ebtnCrearCarrera.addEventListener('click',function (evento) {
+  
+  alert('hola');
+  evento.preventDefault();
+  var camposVacios=validarCamposLlenos(aInputs, eError, 'Todos los campos deben estar llenos');
+    
+ 
+  });
+
+
+
+
 function soloGuion(e){
-       key = e.keyCode || e.which;
-       tecla = String.fromCharCode(key).toLowerCase();
-       caracteresAdmitidos = "áéíóúabcdefghijklmnñopqrstuvwxyz1234567890-";
-       especiales = "8-37-39-46";
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = "áéíóúabcdefghijklmnñopqrstuvwxyz1234567890-";
+    especiales = "8-37-39-46";
 
-<<<<<<< HEAD
-       tecla_especial = false
-       for(var i in especiales){
-            if(key == especiales[i]){
-                tecla_especial = true;
-                break;
-            }
-        }
-
-        if(caracteresAdmitidos.indexOf(tecla)==-1 && !tecla_especial){
-            return false;
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]){
+            tecla_especial = true;
+            break;
         }
     }
-=======
 
->>>>>>> origin/master
+    if (letras.indexOf(tecla)==-1 && !tecla_especial) {
+        return false;
+    }
+}
+

c08d1a4dd7f8bdcedb62abcba24ba4761dcab132
Elizabeth Villalobos <villaloboselizabeth@gmail.com>
2014-07-12 22:51:59 -0600

    Creando modal window para confirmacion de acciones.

--- js/common-logic.js
+++ js/common-logic.js
@@ -17,12 +17,77 @@
 // Funciones generales
 // ------------------------------------------
 
+// Agrega un nuevo elemento/nodo al DOM.
+// @pTag: tipo de elemento a crear (div, span, etc.)
+// @pId: ID que va a tener el nuevo elemento.
+// @pClassName: clases que va a tener el nuevo elemento.
+// pText: texto que va a tener el elemento.
+// @pNode: elemento/nodo al que hay que agregar el nuevo elemento
+function addElementToDOM(pTag, pId, pClassName, pText, pNode) { 
+    var pId = pId || '',
+        pClassName = pClassName || '',
+        pText = pText || '',
+        eEl = document.createElement(pTag),
+        eContent = document.createTextNode(pText);
+
+    eEl.id = pId;
+    eEl.className = pClassName;
+    eEl.appendChild(eContent); 
+    pNode.appendChild(eEl);
+}
+
+// Busca el nodo padre mas cercano que tenga la clase 
+// pParentName
+// @pChildObj: elemento del DOM a partir de donde se realiza la busqueda
+// @pParentName: nombre de la clase a buscar
+function closestParentNode(pChildObj, pParentName) {
+    var node = pChildObj.parentNode;
+    while (!hasClass(node, pParentName)) {
+        node = node.parentNode;
+    }
+    return node;
+};
+
 // Devuelve true o false si en un elemento del DOM tiene la clase
 // que se pasa por parametro.
 // @pEl: elemento del DOM a verificar.
 // @pClassName: clase a verificar.
 function hasClass(pEl, pClassName) {
     return pEl.className.indexOf(pClassName) > -1 ? true : false;
+};
+
+// Muestra una ventana modal.
+// Esta funcion utliza tres clases en el HTML:
+//   .js-modal: elemento que abre el modal window.
+//   .js-modal-close: elemento para cerrar el modal window.
+//   .js-modal-window: este es la ventana modal a mostrarse.
+// Ver el pattern library como referencia para el HTML.
+function modalWindow() {
+    var eModalItems = document.querySelectorAll('.js-modal'),
+        eModalCloseItems = document.querySelectorAll('.js-modal-close'),
+        eBody = document.querySelector('body');
+
+    // Elementos que abren el modal window
+    for (var modal=0; modal < eModalItems.length; modal++) {
+        eModalItems[modal].addEventListener('click', function(event) {
+            event.preventDefault();
+            var sModalId = event.currentTarget.dataset.modalId,
+                eModalWindow = document.querySelector('#' + sModalId);
+            eModalWindow.className = eModalWindow.className.trim() + ' visible';
+            addElementToDOM('div', 'overlay', 'overlay', '', eBody);
+        });
+    }
+    // Elementos que cierran el modal window
+    for (var modalClose=0; modalClose < eModalCloseItems.length; modalClose++) {
+        eModalCloseItems[modalClose].addEventListener('click', function(event) {
+            event.preventDefault();
+            var eModalWindow = closestParentNode(event.currentTarget, 'js-modal-window'),
+                eOverlay = document.querySelector('#overlay');
+
+            eModalWindow.className = eModalWindow.className.replace('visible', '').trim();
+            eBody.removeChild(eOverlay);
+        });
+    }
 };
 
 // Validar que solo puedan introducirse letras
@@ -57,12 +122,12 @@ function soloLetras(e){
 function toggleClass(pEl) {
     if (hasClass(pEl, 'collapsed')) {
         if (pEl.className.indexOf(' collapsed') != -1) {
-            pEl.className = pEl.className.replace('collapsed', '');
+            pEl.className = pEl.className.replace('collapsed', '').trim();
         }
         pEl.className += ' expanded';        
     } else {
         if (pEl.className.indexOf('expanded') != -1) {
-            pEl.className = pEl.className.replace('expanded', '');
+            pEl.className = pEl.className.replace('expanded', '').trim();
         }
         pEl.className += ' collapsed';
     }
@@ -132,4 +197,10 @@ for (var i=0; i < eAccordionItems.length; i++) {
             toggleClass(event.currentTarget.parentNode);
         });
     }
-}
+};
+
+// Inicializar modal windows
+// ------------------------------------------
+modalWindow();
+
+

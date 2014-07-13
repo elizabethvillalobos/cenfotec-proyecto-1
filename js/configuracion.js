ac9874a6050e0934d51b96758ae44cbb5c6c9d4e
joalcego <jo_cego@hotmail.com>
2014-07-13 12:36:02 -0600

    Merge branch 'master' of https://github.com/ewolves/cenfotec-proyecto-1
    Conflicts:
    	css/pages/citas.css

--- js/configuracion.js
+++ js/configuracion.js
@@ -1,12 +1,14 @@
-var ebtnCrear = document.querySelector('#btnCrearCurso'),
+var ebtnCrearCurso = document.querySelector('#btnCrearCurso'),
 	aInputs = document.querySelectorAll('form .form-control'),
 	eError = document.querySelector('.alert-error'),
-	regis=false
+	regis=false,
 	ecodigo = document.querySelector('#txtCodCurso');
 
 /* validar crear curso */
 
-ebtnCrear.addEventListener('click',function (evento) {
+
+ebtnCrearCurso.addEventListener('click',function (evento) {
+
 	
 	evento.preventDefault();
     var camposVacios=validarCamposLlenos(aInputs, eError, 'Todos los campos deben estar llenos');
@@ -14,4 +16,25 @@ ebtnCrear.addEventListener('click',function (evento) {
  
 	});
 
+
+
+
+function soloGuion(e){
+    key = e.keyCode || e.which;
+    tecla = String.fromCharCode(key).toLowerCase();
+    letras = "áéíóúabcdefghijklmnñopqrstuvwxyz1234567890-";
+    especiales = "8-37-39-46";
+
+    tecla_especial = false
+    for(var i in especiales) {
+        if(key == especiales[i]){
+            tecla_especial = true;
+            break;
+        }
+    }
+
+    if (letras.indexOf(tecla)==-1 && !tecla_especial) {
+        return false;
+    }
+}
 

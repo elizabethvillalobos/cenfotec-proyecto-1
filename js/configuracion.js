c757a8edbdc6eb68d1283209c3901e32f5931cf9
diegobari06 <diegobari06@hotmail.com>
2014-07-13 01:30:31 -0600

    registrarCurso

--- js/configuracion.js
+++ js/configuracion.js
@@ -14,4 +14,21 @@ ebtnCrear.addEventListener('click',function (evento) {
  
 	});
 
+function soloGuion(e){
+       key = e.keyCode || e.which;
+       tecla = String.fromCharCode(key).toLowerCase();
+       caracteresAdmitidos = "áéíóúabcdefghijklmnñopqrstuvwxyz1234567890-";
+       especiales = "8-37-39-46";
 
+       tecla_especial = false
+       for(var i in especiales){
+            if(key == especiales[i]){
+                tecla_especial = true;
+                break;
+            }
+        }
+
+        if(caracteresAdmitidos.indexOf(tecla)==-1 && !tecla_especial){
+            return false;
+        }
+    }

ae4429c92f12ce70d3abd4fc825b54418d775bac
Elizabeth Villalobos <villaloboselizabeth@gmail.com>
2014-07-14 14:49:57 -0600

    Arreglando issue de los radio inputs en evaluarCita.html

--- js/evaluacion.js
+++ js/evaluacion.js
@@ -1,6 +1,23 @@
-document.getElementById('no').onclick = 
-function mostrarForm(){
-  document.getElementById('wrapperItems');
-  wrapperItems.classList.remove("mostrarForm");
-  wrapperItems.classList.add("ocultarForm");            
+// document.getElementById('no').onclick = 
+// function mostrarForm(){
+//   document.getElementById('wrapperItems');
+//   wrapperItems.classList.remove("mostrarForm");
+//   wrapperItems.classList.add("ocultarForm");            
+// }
+
+var eRadiosChecked = document.querySelectorAll('.radio');
+if (eRadiosChecked) {
+	for(var i=0; i < eRadiosChecked.length; i++) {
+		eRadiosChecked[i].addEventListener('click', function(event) {
+			var eEl = event.currentTarget,
+				eElParent = closestParentNode(eEl, 'accordion-detail'),
+				eWrapperItems = eElParent.querySelector('.wrapperItems');
+
+			if (eEl.querySelector('input[type="radio"]').value == 'si') {
+				eWrapperItems.className += ' visible';
+			} else {
+				removeClass(eWrapperItems, 'visible');
+			}
+		});
+	}
 }

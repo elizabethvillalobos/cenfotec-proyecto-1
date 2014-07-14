5fc945076cdbb4e8422ec83aeb3e0290bf2532ab
KuzuriX <cotto527@gmail.com>
2014-07-13 22:46:43 -0600

    Agrgando consultarUusario

---
+++ seguridad/registrarUsuario.js
@@ -0,0 +1,23 @@
+var eBtnRegistrar = document.querySelector('#btnRegistrar'),
+    aInputs = document.querySelectorAll('form .form-control'),
+    eError = document.querySelector('.alert-error'),
+    regis=false;
+
+eBtnRegistrar.addEventListener('click', function (evento) {
+    
+    alert('hola');
+    alert('k ase');
+   /* evento.preventDefault();
+    var camposVacios=validarCamposLlenos(aInputs, eError, 'Todos los campos deben estar llenos');
+    if(!camposVacios){
+        var correoCorrecto=validarCorreo(eCorreo, eError, 'El correo es incorrecto');
+    }
+    
+    if(correoCorrecto){
+        document.querySelector('#login').submmit;
+    }
+    */
+    
+
+    
+}); /*addEvenListener = Observador del evento y function ejecuta la funcion dentro de  los corchetes cuando se ejecuta la funcion*/

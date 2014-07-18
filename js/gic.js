974ceb41f0d34d14372eeb41363acb309a8e66af
Elizabeth Villalobos <villaloboselizabeth@gmail.com>
2014-07-18 06:27:57 -0600

    Mas cambios para arreglar links rotos y afinar el estilo.

--- js/gic.js
+++ js/gic.js
@@ -1,7 +1,18 @@
+// ------------------------------------------
+// Inializar funcionalidad de Flat-UI (no modificar)
+// ------------------------------------------
 (function($) {
-    $("select").selectpicker({style: 'btn-hg btn-primary', menuStyle: 'dropdown-inverse'});
+	var selects = $('select');
+	if (selects.length) {
+		$('select').selectpicker({style: 'btn-hg btn-primary', menuStyle: 'dropdown-inverse'});
+	}
     
-    $(".btn-group").on('click', "a", function() {
-      $(this).siblings().removeClass("active").end().addClass("active");
-    });
+    var btnGroud = $('.btn-group');
+    if (btnGroud.length) {
+    	$('.btn-group').on('click', 'a', function() {
+      	$(this).siblings().removeClass('active').end().addClass('active');
+    	});
+	}
+
+	$('.datepicker').datepicker();
 })(jQuery);

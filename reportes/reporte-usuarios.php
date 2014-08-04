c9392d812802de6e7871510070c1a07c4e1d2bc7
joalcego <jo_cego@hotmail.com>
2014-08-03 19:48:12 -0600

    Módulo de reportes

--- reportes/reporte-usuarios.php
+++ reportes/reporte-usuarios.php
@@ -19,7 +19,7 @@
 				<nav class="secondary-nav">
 					<ul class="sec-nav-category">
 						<li class="accordion-item">
-							<a href="reportes.php" class="active">Ranking</a>
+							<a href="reportes.php">Ranking</a>
 						</li>
 						<li class="accordion-item">
 							<a href="reporte-solicitudes.php">Solicitudes</a>
@@ -28,7 +28,7 @@
 							<a href="reporte-agendas.php">Agendas</a>
 						</li>
 						<li class="accordion-item">
-							<a href="reporte-usuarios.php">Usuarios registrados</a>
+							<a href="reporte-usuarios.php" class="active">Usuarios registrados</a>
 						</li>
 					</ul>
 				</nav>
@@ -36,59 +36,66 @@
 
 			<main>
 				<div class="mod-hd">
-					<h2>Reporte de ranking</h2>
+					<h2>Reporte de usuarios</h2>
 					<table>
 						<thead>
 							<tr>
-								<th class="center">Posición</th>
 								<th class="center">Usuario</th>
 								<th class="center">Rol</th>
-								<th class="center">Total de Citas</th>
-								<th class="center">Puntuación Total</th>
+								<th class="center">Carrera</th>
+								<th class="center">Correo electrónico</th>
+								<th class="center">Total de citas</th>
+								<th class="center">Estado</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
-								<td class="center">1</td>
-								<td>Diego Barillas Valverde</td>
-								<td>Estudiante</td>
-								<td class="center">10</td>
-								<td class="center">10</td>
-							</tr>
-							<tr>
-								<td class="center">2</td>
-								<td>Pablo Monestel</td>
-								<td>Coordinador Académico</td>
-								<td class="center">5</td>
-								<td class="center">10</td>
-							</tr>
-							<tr>
-								<td class="center">3</td>
-								<td>Antonio Luna</td>
-								<td>Profesor</td>
-								<td class="center">10</td>
-								<td class="center">9</td>
-							</tr>
-							<tr>
-								<td class="center">4</td>
-								<td>Miguel Coto</td>
-								<td>Estudiante</td>
-								<td class="center">2</td>
-								<td class="center">8</td>
-							</tr>
-							<tr>
-								<td class="center">5</td>
 								<td>Alexander Corrales Solís</td>
 								<td>Estudiante</td>
-								<td class="center">1</td>
-								<td class="center">8</td>
+								<td>Ingeniería del Software</td>
+								<td>acorraless@ucenfotec.ac.cr</td>
+								<td class="center">10</td>
+								<td>Inactivo</td>
 							</tr>
 							<tr>
-								<td class="center">6</td>
 								<td>Andrea Arroyo Cascante</td>
 								<td>Asistente de curso</td>
+								<td>Ingeniería del Software</td>
+								<td>aarroyoc@ucenfotec.ac.cr</td>
+								<td class="center">1</td>
+								<td>Inactivo</td>
+							</tr>
+							<tr>
+								<td>Antonio Luna</td>
+								<td>Profesor</td>
+								<td>Ingeniería del Software</td>
+								<td>aluna@ucenfotec.ac.cr</td>
+								<td class="center">6</td>
+								<td>Activo</td>
+							</tr>
+							<tr>
+								<td>Diego Barillas Valverde</td>
+								<td>Estudiante</td>
+								<td>Ingeniería del Software</td>
+								<td>dbarillasv@ucenfotec.ac.cr</td>
+								<td class="center">10</td>
+								<td>Activo</td>
+							</tr>
+							<tr>
+								<td>Miguel Coto</td>
+								<td>Estudiante</td>
+								<td>Ingeniería del Software</td>
+								<td>mcoto@ucenfotec.ac.cr</td>
 								<td class="center">5</td>
-								<td class="center">5</td>
+								<td>Activo</td>
+							</tr>
+							<tr>
+								<td>Pablo Monestel</td>
+								<td>Coordinador Académico</td>
+								<td>Desarrollo Web</td>
+								<td>pmonestel@ucenfotec.ac.cr</td>
+								<td class="center">8</td>
+								<td>Activo</td>
 							</tr>
 							
 						</tbody>

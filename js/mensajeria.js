b1eaa87f03e505caa0dd813af0177a2c30de2dd4
joalcego <jo_cego@hotmail.com>
2014-08-16 23:37:32 -0600

    Mensajeria HTML/CSS

---
+++ js/mensajeria.js
@@ -0,0 +1,66 @@
+//busqueda de cursos
+function buscarDestinatario(evento){
+    var resDestinatario = document.querySelector('#resDestinatario'),
+    input = document.querySelector('#txtDestinatario');
+	autocompletar(resDestinatario, input, obtenerDestinatarios()[0], obtenerDestinatarios()[1]);
+}
+
+var rDestinatarios=document.querySelector('#resDestinatario');
+if(rDestinatarios){
+	rDestinatarios.addEventListener('click', function(e) {
+		var input = document.querySelector('#txtDestinatario');
+		reemplazarTextoInput(rDestinatarios,input,e.target, "idDestinatario");		
+	});
+}
+
+//obtener destinatarios
+function obtenerDestinatarios() {	
+	var resultados=[];
+	
+	// Solicitar datos al servicio.
+	$.ajax({
+		url: '../includes/service-usuarios.php',
+		type: 'get', // Se utiliza get por vamos a obtener datos, no a postearlos.
+		data: { // Objeto con los parámetros que utiliza el servicio.
+			'query': 'consultarDestinatarios',
+			'idRemitente': location.search.split("=")[1]
+		},
+		dataType: 'json',
+		success: function(response){
+			var respuesta=$.parseJSON(response.data);
+			var nombres =[];
+			var ids = [];
+			for (var i=0; i<respuesta.destinatarios.length; i++)
+			{
+				nombres.push(respuesta.destinatarios[i].nombre);
+				ids.push(respuesta.destinatarios[i].id);
+			}
+			resultados.push(nombres);
+			resultados.push(ids);
+		},
+		error: function(response){
+			resultados=null;
+		},
+		async: false
+	});
+	return resultados;
+};
+
+
+//enviar formulario
+var btnEnviar=document.querySelector('#btnEnviar');
+if(btnEnviar!=null){
+	btnEnviar.addEventListener('click',function(event){
+		limpiarMensajesError();
+		var txtDestinatario = document.querySelector('#txtDestinatario');
+		if(txtDestinatario.value.trim()==''){
+			mostrarMensajeError(txtDestinatario, "Este campo no puede estar vacío.");
+		}
+		
+		var txtMensaje = document.querySelector('#txtMensaje');
+		if(txtMensaje.value.trim()==''){
+			mostrarMensajeError(txtMensaje, "Este campo no puede estar vacío.");
+		}
+	});
+}
+
--- mensajeria/mensajeria.php
+++ mensajeria/mensajeria.php
@@ -10,6 +10,7 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="/cenfotec-proyecto-1/css/bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="/cenfotec-proyecto-1/css/gic.css">
+		<link rel="stylesheet" href="/cenfotec-proyecto-1/css/pages/mensajeria.css">
 	</head>
 	<body>
 		<div class="wrapper">
@@ -37,12 +38,40 @@
 			</aside>
 
 			<main>
-				<div class="mod-hd">
-					<h2>Conversaciones</h2>
-				</div>
-				<div class="mod-bd">
+				<section>
 					
-				</div>
+					<div class="conversacion">
+						<div class="mod-hd">
+							<h2>Antonio Luna</h2>
+						</div>
+						<div class="mod-bd mensajes">
+							<div class="form-row">
+								<span>Jose Cerdas</span>
+								<p>Hola profe necesito que me ayude</p>
+								<p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que ta</p>
+							</div>
+							<div class="form-row noLeido">
+								<span>Antonio Luna</span>
+								<p>Y que quiere que haga yo?</p>
+							</div>
+							<div class="form-row noLeido">
+								<span>Jose Cerdas</span>
+								<p>Que me explique todo de nuevo</p>
+							</div>
+						</div>
+						<form id="nuevoMensaje" action="#" method="post">		
+							<div class="form-row">
+								<label for="txtMensaje">Nuevo mensaje:</label>
+								<textarea id="txtMensaje" placeholder="Ingrese el mensaje" class="form-control"></textarea>
+							</div>
+							<div id="enviarRow" class="form-row form-row-button">
+								<button id="btnEnviar" class="btn btn-primary">Enviar</button>
+							</div>
+						</form>
+					</div>
+					
+					
+				</section>
 			</main>
 			
 			<?php include(ROOT.'/includes/footer.php'); ?>

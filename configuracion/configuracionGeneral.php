133d548bb5e18b865800d2cfa453ab2dac053587
ozzman02 <osantamaria@gmail.com>
2014-08-22 07:21:30 -0600

    ConfiguracionGeneral
    Borrar archivos innecesarios. Diego los había hecho

--- configuracion/configuracionGeneral.php
+++ configuracion/configuracionGeneral.php
@@ -1,3 +1,10 @@
+<?php
+    require_once('../includes/functions.php');  
+
+    $currentModule = 'configuracion';
+    $currentSubModule = 'general';
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -10,180 +17,83 @@
 	</head>
 	<body>
 		<div class="wrapper">
-			<header>
-				<a href="../index.php" class="home">
-					<h1 class="logo">Gestor Inteligente de Citas</h1>
-				</a>
-
-				<!-- Menu principal -->
-			<nav class="main-nav">
-					<ul>
-						<li>
-							<a href="../citas/citas.php" class="citas flaticon-calendar68"><span>Citas</span></a>
-						</li>
-						<li>
-							<a href="../evaluacion/miRanking.php" class="evaluacion  flaticon-verification5"><span>Evaluación</span></a>
-						</li>
-						<li>
-							<a href="../mensajeria/mensajeria.php" class="mensajeria flaticon-black218"><span>Mensajería</span></a>
-						</li>
-						<li>
-							<a href="../reportes/reportes.php" class="reportes flaticon-seo2"><span>Reportes</span></a>
-						</li>
-						<li>
-							<a href="configuracion/perfil.php" class="configuracion active flaticon-machine2"><span>Configuración</span></a>
-						</li>
-					</ul>
-				</nav>
-
-				<section class="busqueda">
-					<input id="q" type="text" value="" placeholder="Buscar personas" />
-					<button id="btnBuscar" class="flaticon-magnifier12" type="submit"></button>
-				</section>
-
-				<section class="usr-info">
-					<span class="usr-greeting">Bienvenido, Álvaro!</span>
-					<img class="usr-photo" src="../images/users/default-user.png" width="40" height="40">
-					<ul>
-						<li>
-							<a href="../configuracion/perfil.php" class="usr-editar-perfil">Mi cuenta</a>
-						</li>
-						<li>
-							<a href="../seguridad/iniciarSesion.php" class="usr-cerrar-sesion">Cerrar sesión</a>
-						</li>
-					</ul>
-				</section>
-			</header>
-
-			<aside>
-
-				<nav class="secondary-nav">
-					<ul class="sec-nav-category">
-						<li class="accordion-item">
-                            <a href="perfil.php">Perfil</a>
-
-                        </li>
-                        <li class="accordion-item">
-                            <a href="#">Mi cuenta</a>
-                        </li>
-                        <li class="accordion-item">
-
-                            <a href="consultarCarreras.php">Carreras y cursos</a>
- 						</li>
-                        <li class="accordion-item">
-                            <a href="consultarUsuario.php">Usuarios</a>
-                        </li>
-                        <li class="accordion-item">
-                            <a href="configuracionGeneral.php" class="active">General</a>
-                        </li>
-
-					</ul>
-				</nav>
-			</aside>
-
+			<?php include(ROOT.'/includes/header.php'); ?>
+			<?php include(ROOT.'/includes/aside-configuracion.php'); ?>
 
 			<main id="mainCG">
 				<ul class="accord">
-                        <li class="accordion-item expanded">
-                            <a href="#">Citas</a>
-                            <ul class="accordion-detail">
-                            	<div>
-                            		<form class="frmC" action="configuracionGeneralConfirm.php" method="post">
-
-	                            		<div id="divTxtCit">
-	                            			<label class="lbl">Dias de expiración de solicitud:</label>
-		                            	</div>
-
-		                            	<div id="divNbr">
-		                            		<input class="nbr" type="number" name="puntaje" min="1" max="31" value="30">
-		                            	</div>
-
-		                            	<div  id="divBtnEvr">
-											<button class="btn btn-primary" type="submit">Aplicar</button>
-			                                
-							            </div>
-
-	                                </form>
-                            	</div>
-                            </ul>
-                        </li>
-                        <li class="accordion-item expanded">
-                            <a href="#">Mensajería</a>
-                            <div class="accordion-detail">
-
-                            	<div>
-                            		<form class="frmC" action="configuracionGeneralConfirm.php" method="post">
-
-	                            		<div id="divTxtCit">
-	                            			<label class="lbl">Máximo carateres por mensaje:</label>
-		                            	</div>
-
-		                            	<div id="divNbr">
-		                            		<input class="nbr" type="number" name="puntaje" min="1" max="31" value="30">
-		                            	</div>
-
-		                            	<div  id="divBtnEvr">
-											<button class="btn btn-primary" type="submit">Aplicar</button>
-			                                
-							            </div>
-
-	                                </form>
-                            	</div>                                
-
+                    <li class="accordion-item expanded">
+                       <a href="#">Citas</a>
+                        <ul class="accordion-detail">
+                            <div>
+                            	<form class="frmC" action="configuracionGeneralConfirm.php" method="post">
+	                            	<div id="divTxtCit">
+	                            		<label class="lbl">Dias de expiración de solicitud:</label>
+		                            </div>
+		                            <div id="divNbr">
+		                            	<input class="nbr" type="number" name="puntaje" min="1" max="31" value="30">
+		                            </div>
+		                            <div  id="divBtnEvr">
+										<button class="btn btn-primary" type="submit">Aplicar</button>
+							        </div>
+	                            </form>
                             </div>
-                        </li>
-                        <li class="accordion-item expanded">
-                            <a href="#">Notificaciones</a>
-                            <div class="accordion-detail">
-
-                            	<div>
-                            		<form id="form-confGrl" action="configuracionGeneralConfirm.php" method="post" >
-
-	                            		<div id="divTxtMail">
-	                            			<label class="lbl">Servidor smtp:<input id="smtpSvr" type="text" placeholder="smtp" class="form-control" /></label>
-	                            			
-		                            	</div>
-
-		                            	<div id="divMail">
-		                            		<label class="lbl">Puerto:</label>
-		                            		<input id="port" type="text" placeholder="puerto" class="form-control" />
-		                            			                            		
-		                            	</div>
-
-		                            	<div id="divMail">
-		                            		<label class="lbl">Smtp seguridad:</label>
-		                            		<input id="ssl" type="text" placeholder="ssl" class="form-control" />
-		                            			                            		
-		                            	</div>
-
-		                            	<div id="divMail">
-		                            		<label class="lbl">Usuario (dirección de correo):</label>
-		                            		<input id="emailNot" type="text" placeholder="mail@ucenfotec.ac.cr" class="form-control" />
-		                            		<p class="alert-error"></p>		                            		
-		                            	</div>
-
-		                            	<div id="divMail">
-		                            		<label class="lbl">Clave:</label>
-		                            		<input id="pass" type="password" placeholder="clave" class="form-control" />
-		                            				                            		
-		                            	</div>
-
-		                            	<p class="alert-error1"></p>
-
-		                            	<div  id="divBtnEvr">
-											<button id="btnEvr"class="btn btn-primary" type="submit">Aplicar</button>			                                   
-							            </div>							            
-	                                </form>
-                            	</div> 
-                            	                               
-
-                            </div>
-                        </li>
-                    </ul>
-                 </main>   
-			<footer>
-				<p>2014 Universidad Cenfotec. Todos los derechos reservados.</p>
-			</footer>
+                        </ul>
+                    </li>
+                    <li class="accordion-item expanded">
+                        <a href="#">Mensajería</a>
+                        <div class="accordion-detail">
+                        	<div>
+                            	<form class="frmC" action="configuracionGeneralConfirm.php" method="post">
+	                            	<div id="divTxtCit">
+	                            		<label class="lbl">Máximo carateres por mensaje:</label>
+		                            </div>
+		                            <div id="divNbr">
+		                            	<input class="nbr" type="number" name="puntaje" min="1" max="31" value="30">
+		                            </div>
+		                            <div  id="divBtnEvr">
+										<button class="btn btn-primary" type="submit">Aplicar</button>        
+							        </div>
+	                            </form>
+                           	</div>                                
+                        </div>
+                    </li>
+                    <li class="accordion-item expanded">
+                        <a href="#">Notificaciones</a>
+                        <div class="accordion-detail">
+                        	<div>
+                            	<form id="form-confGrl" action="configuracionGeneralConfirm.php" method="post" >
+	                            	<div id="divTxtMail">
+	                            		<label class="lbl">Servidor smtp:<input id="smtpSvr" type="text" placeholder="smtp" class="form-control" /></label>	
+		                            </div>
+		                            <div id="divMail">
+		                            	<label class="lbl">Puerto:</label>
+		                            	<input id="port" type="text" placeholder="puerto" class="form-control" />                           		
+		                            </div>
+		                            <div id="divMail">
+		                            	<label class="lbl">Smtp seguridad:</label>
+		                            	<input id="ssl" type="text" placeholder="ssl" class="form-control" />		                            		
+		                           	</div>
+		                            <div id="divMail">
+		                            	<label class="lbl">Usuario (dirección de correo):</label>
+		                            	<input id="emailNot" type="text" placeholder="mail@ucenfotec.ac.cr" class="form-control" />
+		                            	<p class="alert-error"></p>		                            		
+		                            </div>
+		                            <div id="divMail">
+		                            	<label class="lbl">Clave:</label>
+		                            	<input id="pass" type="password" placeholder="clave" class="form-control" />			                            		
+		                            </div>
+		                            <p class="alert-error1"></p>
+		                            <div  id="divBtnEvr">
+										<button id="btnEvr"class="btn btn-primary" type="submit">Aplicar</button>			                                   
+							        </div>							            
+	                            </form>
+                            </div>                                
+                        </div>
+                    </li>
+                </ul>
+            </main>   
+			<?php include(ROOT.'/includes/footer.php'); ?>
 		</div>
 		<!-- Load JS-->
 		<script src="/cenfotec-proyecto-1/js/vendors/jquery-1.8.3.min.js"></script>

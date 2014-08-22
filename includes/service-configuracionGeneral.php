d19ed7468f0bd3e93decfafeefa9b524f8d0e2ee
ozzman02 <osantamaria@gmail.com>
2014-08-22 13:28:08 -0600

    Pruebas
    ConfigGeneral

---
+++ includes/service-configuracionGeneral.php
@@ -0,0 +1,71 @@
+<?php
+	error_reporting(0);
+	require_once('functions.php');
+
+	header('Content-Type:application/json');
+
+	if (!empty($_GET['query'])) {
+		$queryType = $_GET['query'];
+
+		switch ($queryType) {
+			case 'modificarDiasDeExpiracion':
+				modificarDiasDeExpiracion();
+				break;
+			/*case 'modificarCaracteres':
+				modificarCaracteres();
+				break;
+			case 'modificarCorreoClave':
+				modificarCorreoClave();
+				break;*/
+		}
+	} else {
+		deliver_response(400, 'Bad request', NULL);
+	}
+
+	function modificarDiasDeExpiracion(){
+		if (!empty($_GET['pdias'])){
+			$dias = utf8_decode($_GET['pdias']);
+			$query = "UPDATE configuracion SET valor='$dias' WHERE parametro='diasExpiracion'";
+			$result = do_query($query);
+			deliver_response(200, 'OK', 'Registrado con exito');
+		}else{
+			deliver_response(400, 'Bad request', NULL);
+		}
+	}
+
+	function modificarCaracteres(){
+		if (!empty($_GET['pcaracteres'])){
+			$caracteres = utf8_decode($_GET['pcaracteres']);
+			$query = "UPDATE configuracion SET valor='$caracteres' WHERE parametro='caracteresMaximos'";
+			$result = do_query($query);
+			deliver_response(200, 'OK', 'Registrado con exito');
+		}else{
+			deliver_response(400, 'Bad request', NULL);
+		}
+	}
+
+	function modificarCorreoClave(){
+		if (!empty($_GET['pcorreo']) && !empty($_GET['pclave'])){
+			$correo = utf8_decode($_GET['pcorreo']);
+			$clave = utf8_decode($_GET['pclave']);
+			$query = "UPDATE configuracion SET valor='$correo' WHERE parametro='correoNotificacion'";
+			$result = do_query($query);
+			$query = "UPDATE configuracion SET valor='$clave' WHERE parametro='contrasena'";
+			$result = do_query($query);
+			deliver_response(200, 'OK', 'Registrado con exito');
+		}else{
+			deliver_response(400, 'Bad request', NULL);
+		}
+	}
+
+	function deliver_response($status, $statusMessage, $data) {
+		header("HTTP/1.1 $status $statusMessage");
+		$response['status'] = $status;
+		$response['status-message'] = $statusMessage;
+		$response['data'] = $data;
+		echo json_encode($response);
+	} 
+
+
+
+?>
--- js/configuracionGeneral.js
+++ js/configuracionGeneral.js
@@ -1,90 +1,39 @@
-ebtnModificarCorreo=document.querySelector('#btnEvr');
-if(ebtnModificarCorreo){
-ebtnModificarCorreo.addEventListener('click',function () {
-	
-
-	var correoNotificacion = $('#emailNot').val();
-	var hayError = false;
-		if(correoNotificacion == ""){
-    	mostrarMensajeError(document.querySelector('#emailNot'),"El nombre de la carrera no puede ser igual al código.");
-		hayError=true;
-    } 
-    if(!hayError){
-	var request = $.ajax({
-		url: "/cenfotec-proyecto-1/includes/functions-configuracionGeneral.php",
-		type: "get",
+function actualizarContrasena(pdias){
+	$.ajax({
+		url: '../includes/service-configuracionGeneral.php',
+		type: 'get',
 		data: {
-                'query' : 'modificarCorreo',
-			   'pcorreoNotificacion':correoNotificacion
-			  },
-		datatype: 'json',
-		success: function(data){    
-			
-			console.log('exitooooo');
-
-		}
+				'query': 'modificarDiasDeExpiracion',
+				'pdias': pdias
+			},
+			datatype: 'json',
+			success: function(response){
+				console.log(response);
+				//window.location = "/cenfotec-proyecto-1/configuracion/cambiarContrasena-confirmar.php";
+			},
+			error: function(response){
+				console.log(response);
+				console.log("error");
+			}
 	});
 }
-});
-}
 
-ebtnModificarCaracteres=document.querySelector('#modificarCaracteres');
-if(ebtnModificarCaracteres){
-ebtnModificarCaracteres.addEventListener('click',function () {
-	
 
-	var caracterMaximo = $('#caracterMaximo').val();
-	var hayError = false;
-		if(caracterMaximo == ""){
-    	mostrarMensajeError(document.querySelector('#caracterMaximo'),"El nombre de la carrera no puede ser igual al código.");
-		hayError=true;
-    } 
-    if(!hayError){
-	var request = $.ajax({
-		url: "/cenfotec-proyecto-1/includes/functions-configuracionGeneral.php",
-		type: "get",
-		data: {
-                'query' : 'modificarCaracter',
-			   'pcaracterMaximo':caracterMaximo
-			  },
-		datatype: 'json',
-		success: function(data){    
-			
-			console.log('exitooooo');
 
-		}
-	});
-}
-});
-}
-
-ebtnModificarDias=document.querySelector('#guardarDias');
-if(ebtnModificarDias){
-ebtnModificarDias.addEventListener('click',function () {
-	
-
-	var diasExpiracion = $('#dias').val();
-	var hayError = false;
+(function($) {
+	$('#modificarDias').click(function(){
+		var diasDeExpiracion = $('#diasExpiracion').val();
+		
 		if(diasExpiracion == ""){
-    	mostrarMensajeError(document.querySelector('#dias'),"");
-		hayError=true;
-    } 
-    if(!hayError){
-	var request = $.ajax({
-		url: "/cenfotec-proyecto-1/includes/functions-configuracionGeneral.php",
-		type: "get",
-		data: {
-                'query' : 'modificarDias',
-			   'pdias':diasExpiracion
-			  },
-		datatype: 'json',
-		success: function(data){    
-			
-			console.log('exitooooo');
-
+			mostrarMensajeError(document.querySelector('#diasExpiracion'),"Este campo no puede estar vacío y solo acepta números");
+		}else{
+			modificarDias(diasDeExpiracion);
 		}
+
+		
+			
 	});
-}
-});
-}
+
+	
+})(jQuery);	
 
